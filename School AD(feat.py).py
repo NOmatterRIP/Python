b7a14d37fc18e5e38839cbbc3430213a534724e8
Kim Youngrae <kyrae0519@gmail.com>
2022-09-22 14:59:53 +0900

    Create School AD(feat.py).py
    School AD (feat. python)

---
+++ School AD(feat.py).py
@@ -0,0 +1,12 @@
+region = "부산 남구 용호동"
+sc = "동명대학교"
+number_of_build = "15"
+major = "Information security"
+
+print("#####################")
+print("#                   #")
+print("#   학교 입학 홍보  #")
+print("#                   #")
+print("#####################")
+print("")
+print(region, "에 위치한 아주 좋은 ", sc, "에서 학생을 모집합니다. 이 ",sc,"는 ", number_of_build, "의 학과가 위치해 있으며 학과명은", major,"입니다.")
